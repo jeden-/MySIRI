64ef0d27bb5c74511af33bab1c37c69f606744c3
m <jeden-@users.noreply.github.com>
2024-07-10 22:14:52 +0200

    Initial Commit

---
+++ MySIRI/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MySIRI
+//
+//  Created by Mariusz on 10/07/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ MySIRITests/MySIRITests.swift
@@ -0,0 +1,17 @@
+//
+//  MySIRITests.swift
+//  MySIRITests
+//
+//  Created by Mariusz on 10/07/2024.
+//
+
+import Testing
+@testable import MySIRI
+
+struct MySIRITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
